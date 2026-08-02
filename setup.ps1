1f7f398a7274035c9bbf09888738f8b18ac29c30
MAJD <majd9alharbi@gmail.com>
2026-08-02 18:23:35 +0300

    main file

---
+++ setup.ps1
@@ -0,0 +1,13 @@
+Write-Host "Welcome to the startup project"
+mkdir src
+ni src\main.py
+echo "# WebBootcamp3" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/MajdAlharbi/WebBootcamp3.git
+git push -u origin main
+Write-Host "All complete!!"
+
+
